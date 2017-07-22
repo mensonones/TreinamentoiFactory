40f490794099fe4c700688084bd8c7625b4afeab
Emerson Vieira <mensones.1@gmail.com>
2017-07-22 01:54:40 -0300

    cors class filter

--- src/main/java/br/ufc/demo1/author/AuthorRepository.java
+++ src/main/java/br/ufc/demo1/author/AuthorRepository.java
@@ -2,15 +2,10 @@ package br.ufc.demo1.author;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import java.util.Collection;
-import java.util.List;
 
 /**
  * Created by emerson on 17/07/2017.
  */
-@CrossOrigin
 @Repository
 public interface AuthorRepository extends CrudRepository<Author, Integer>{
 }
--- src/main/java/br/ufc/demo1/config/WebConfig.java
+++ src/main/java/br/ufc/demo1/config/WebConfig.java
@@ -1,20 +1,41 @@
 package br.ufc.demo1.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+import org.springframework.stereotype.Component;
 
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://treinamento-ifactory.herokuapp.com/")
-                .allowedMethods("PUT", "DELETE", "POST", "PATCH", "GET")
-                .allowedHeaders("header1", "header2", "header3")
-                .exposedHeaders("header1", "header2")
-                .allowCredentials(false).maxAge(3600);
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class WebConfig  implements Filter {
+
+    public WebConfig() {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+       // response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+       // response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Expose-Headers", "Location");
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
     }
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
 }
